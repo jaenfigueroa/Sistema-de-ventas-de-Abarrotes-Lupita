09254deb27497c6f843eddfc673479853b881325
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-02-06 10:09:19 -0500

    iniciar proyecto

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import frames.DashboardFrame;
+
+public class Main {
+	
+	// Lanzar la aplicación
+
+	public static void main(String[] args) {
+	    try {
+	        DashboardFrame frame = new DashboardFrame();
+	        frame.setVisible(true);
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+
+}
---
+++ src/frames/DashboardFrame.java
@@ -0,0 +1,47 @@
+package frames;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class DashboardFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DashboardFrame frame = new DashboardFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DashboardFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Hola mundo");
+		lblNewLabel.setBounds(156, 115, 112, 13);
+		contentPane.add(lblNewLabel);
+	}
+}
