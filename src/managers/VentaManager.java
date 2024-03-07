94a56bdd56110c7637305b8ef93229d269e3c278
jaenfigueroa <jaenfigueroa03042000@gmail.com>
2024-03-06 19:29:42 -0500

    agregar interface de manager

--- src/managers/VentaManager.java
+++ src/managers/VentaManager.java
@@ -1,8 +1,34 @@
 package managers;
 
+import app.Main;
+import clases.Producto;
 import clases.Venta;
 import clasesPadre.Manager;
 
 public class VentaManager extends Manager<Venta> {
 	
+	public Venta ingresar(int codigoCliente, int codigoProducto, int cantidad) {
+		
+		// double precio = ProductoManager.consultarProducto(codigoProducto).getPrecio();
+		Venta ventaNueva = new Venta(codigoCliente, codigoProducto, cantidad);
+		
+		super.ingresar(ventaNueva);
+		
+		// actualiza el stock actual del producto
+		Producto producto = Main.productoManager.consultar(codigoProducto);
+		
+		
+		int nuevoStockActual = producto.getStockActual() - cantidad;
+		producto.setStockActual(nuevoStockActual);
+		
+		// actualizar la cantidad unidades vendidas
+		int nuevaCantidadVentas = producto.getCantidadVentasAcumuladas() + cantidad;
+		producto.setCantidadVentasAcumuladas( nuevaCantidadVentas );
+		
+		// actualizar la cantidad importe total acumulado
+		double nuevaCantidadImporteAcumulado = producto.getCantidadImporteAcumulado() + ventaNueva.getImporteTotalPagar();
+		producto.setCantidadImporteAcumulado(nuevaCantidadImporteAcumulado);
+		
+		return ventaNueva;
+	}
 }
